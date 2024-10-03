31c7b4c4d80dc12b266b0d0a639db20919b07704
Duyquoclite <97270992+Duyquoclite@users.noreply.github.com>
2024-10-04 02:44:15 +0700

    Create trung bình điểm.c

---
+++ bài tập/trung bình điểm.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+float a, b, c;
+
+int main() {
+  printf("Nhập điểm toán: ");
+  scanf("%f", &a);
+  printf("Nhập điểm lý: ");
+  scanf("%f", &b);
+  printf("Nhập điểm hóa: ");
+  scanf("%f", &c);
+  if (a > 10 || a < 0 || b > 10 && b < 0 || c > 10 || c < 0)
+    return printf("KHÔNG HỢP LỆ");
+  printf("\nĐiểm trung bình: %.2f", (float) (a + b + c) / 3);
+}
